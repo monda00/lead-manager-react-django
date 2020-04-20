84bcce9d9979d700bcb861c71138fae06a701460
monda <monda0524@gmail.com>
2020-04-20 23:18:42 +0900

    add delete action

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_LEADS } from './types';
+import { GET_LEADS, DELETE_LEAD } from './types';
 
 // GET LEADS
 export const getLeads = () => dispatch => {
@@ -11,4 +11,17 @@ export const getLeads = () => dispatch => {
         payload: res.data
       });
     }).catch(err => console.log(err));
+};
+
+//DLETE LEAD
+export const deleteLead = (id) => dispatch => {
+  axios
+    .get(`/api/leads/${id}/`)
+    .then(res => {
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    })
+    .catch(err => console.log(err));
 };
