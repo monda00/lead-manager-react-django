9aa1dbe9553e5f696bc2f5fa29f416279b08ea38
monda <monda0524@gmail.com>
2020-05-01 23:54:23 +0900

    fix eslint error

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -4,17 +4,17 @@ import ReactDOM from 'react-dom';
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 
+import { Provider } from 'react-redux';
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
 import Alerts from './layout/Alerts';
 
-import { Provider } from 'react-redux';
 import store from '../store';
 
 // Alert Options
 const alertOptions = {
   timeout: 3000,
-  position: 'top center'
+  position: 'top center',
 };
 
 class App extends Component {
@@ -22,16 +22,16 @@ class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
+          <>
             <Header />
             <Alerts />
             <div className="container">
               <Dashboard />
             </div>
-          </Fragment>
+          </>
         </AlertProvider>
       </Provider>
-    )
+    );
   }
 }
 
