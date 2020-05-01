f99dff3e94f6933a092f39ce27c0f2b5cfb82ecd
monda <monda0524@gmail.com>
2020-05-02 00:00:24 +0900

    fix eslint error

--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,25 +1,25 @@
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from '../actions/types.js';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from '../actions/types';
 
 const initialState = {
-  leads: []
+  leads: [],
 };
 
-export default function(state = initialState, action) {
-  switch(action.type) {
+export default function (state = initialState, action) {
+  switch (action.type) {
     case GET_LEADS:
       return {
         ...state,
-        leads: action.payload
+        leads: action.payload,
       };
     case DELETE_LEAD:
       return {
         ...state,
-        leads: state.leads.filter(lead => lead.id !== action.payload)
+        leads: state.leads.filter((lead) => lead.id !== action.payload),
       };
     case ADD_LEAD:
       return {
         ...state,
-        leads: [...state.leads, action.payload]
+        leads: [...state.leads, action.payload],
       };
     default:
       return state;
