27cc78d8ffbdcd8c333a719fa9869d6ba3ea6fc6
monda <monda0524@gmail.com>
2020-05-04 20:18:44 +0900

    show error message-

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -11,8 +11,9 @@ export class Alerts extends Component {
   componentDidUpdate(prevProps) {
     const { error, alert } = this.props;
     if (error !== prevProps.error) {
-      if (error.msg.name) alert.error('Name is reequired');
-      if (error.msg.email) alert.error('Email is reequired');
+      if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+      if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
+      if (error.msg.message) alert.error(`Message: ${error.msg.message.join()}`);
     }
   }
 
