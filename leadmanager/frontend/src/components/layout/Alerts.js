037a663d893c51e377464bc87d5859a34b96722f
monda <monda0524@gmail.com>
2020-05-04 20:52:08 +0900

    add add message

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -19,6 +19,7 @@ export class Alerts extends Component {
 
     if (message !== prevProps.message) {
       if (message.deleteLead) alert.success(message.deleteLead);
+      if (message.addLead) alert.success(message.addLead);
     }
   }
 
