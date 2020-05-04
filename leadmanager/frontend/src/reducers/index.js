cf562308d43c65062a78294526eb7a8c47073b69
monda <monda0524@gmail.com>
2020-05-04 20:44:35 +0900

    add delete message

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import leads from './leads';
 import errors from './errors';
+import messages from './message';
 
 export default combineReducers({
   leads,
   errors,
+  messages,
 });
