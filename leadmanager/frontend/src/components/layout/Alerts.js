d3c2afbbcdd38e16580feda0db5ec24c86d73093
monda <monda0524@gmail.com>
2020-05-04 20:13:34 +0900

    add error message

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { withAlert } from 'react-alert';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 export class Alerts extends Component {
-  componentDidMount() {
-    this.props.alert.show('It Works');
+  static propTypes = {
+    error: PropTypes.object.isRequired,
+  };
+
+  componentDidUpdate(prevProps) {
+    const { error, alert } = this.props;
+    if (error !== prevProps.error) {
+      if (error.msg.name) alert.error('Name is reequired');
+      if (error.msg.email) alert.error('Email is reequired');
+    }
   }
 
   render() {
@@ -11,4 +21,8 @@ export class Alerts extends Component {
   }
 }
 
-export default withAlert()(Alerts);
+const mapStateToProps = (state) => ({
+  error: state.errors,
+});
+
+export default connect(mapStateToProps)(withAlert()(Alerts));
