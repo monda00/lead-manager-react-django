24c0ea4c92958678ead76260dd9f1e6e6446bd71
monda <monda0524@gmail.com>
2020-05-10 20:49:34 +0900

    add password not match message

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -21,6 +21,7 @@ export class Alerts extends Component {
     if (message !== prevProps.message) {
       if (message.deleteLead) alert.success(message.deleteLead);
       if (message.addLead) alert.success(message.addLead);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
     }
   }
 
